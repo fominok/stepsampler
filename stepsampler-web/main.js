7d722756f01f72b772c68eb7806b3587bbe76dbf
Evgeny Fomin <fominok@hotmail.com>
2025-09-07 12:15:37 +0200

    this works

--- stepsampler-web/main.js
+++ stepsampler-web/main.js
@@ -1,3 +1,47 @@
+let files = [];
+
+function reset() {
+  files = [];
+  document.getElementById("num-files-selected").textContent = 'No files selected';
+  document.getElementById("start-button").disabled = true;
+}
+
+function showInfo() {
+  document.getElementById("info-alert").style.display = "block";
+  document.getElementById("danger-alert").style.display = "none";
+}
+
+function showDanger() {
+  document.getElementById("danger-alert").style.display = "block";
+  document.getElementById("info-alert").style.display = "none";
+}
+
+function processFiles() {
+  let result;
+  try {
+    result = window.wasmBindings.process_files(files);
+  } catch (error) {
+    console.error(error);
+    showDanger();
+    reset();
+    return;
+  }
+
+  const blob = new Blob([result], { type: "application/octet-stream" });
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement("a");
+  a.href = url;
+
+  a.download = `${files.length}_processed_sample.wav`;
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
+  URL.revokeObjectURL(url);
+  showInfo();
+  reset();
+}
+
 addEventListener("TrunkApplicationStarted", (event) => {
   console.log("application started - bindings:", window.wasmBindings, "WASM:", event.detail.wasm);
 
@@ -14,27 +58,14 @@ addEventListener("TrunkApplicationStarted", (event) => {
   dropzone.addEventListener("drop", async e => {
     e.preventDefault();
 
-    const files = Array.from(e.dataTransfer.files);
-    const buffers = [];
+    const droppedFiles = Array.from(e.dataTransfer.files);
 
-    for (const file of files) {
+    for (const file of droppedFiles) {
       const arrayBuffer = await file.arrayBuffer();
-      buffers.push(new Uint8Array(arrayBuffer));
+      files.push(new Uint8Array(arrayBuffer));
     }
 
-    // Pass the array of Uint8Arrays to WASM once
-    const result = window.wasmBindings.process_files(buffers); // WASM expects JsValue/array of Uint8Array
-
-    // Convert returned bytes into a downloadable file
-    const blob = new Blob([result], { type: "application/octet-stream" });
-    const url = URL.createObjectURL(blob);
-
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = "processed_sample.wav";
-    document.body.appendChild(a);
-    a.click();
-    a.remove();
-    URL.revokeObjectURL(url);
+    document.getElementById("start-button").disabled = false;
+    document.getElementById("num-files-selected").textContent = `${droppedFiles.length} files selected`;
   });
 });
